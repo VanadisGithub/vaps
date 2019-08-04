3999bacd51f8868c3e6fcd3de0a72f6457a84fee
遥远 <yaoyuan@dtstack.com>
2019-08-04 17:30:17 +0800

    fix 精简

--- vap-lang/src/main/java/com/vanadis/lang/http/IPUtils.java
+++ vap-lang/src/main/java/com/vanadis/lang/http/IPUtils.java
@@ -1,5 +1,6 @@
 package com.vanadis.lang.http;
 
+import java.io.*;
 import java.net.InetAddress;
 
 /**
@@ -35,6 +36,16 @@ public class IPUtils {
      */
     public static String getLocalIP() throws Exception {
         return InetAddress.getLocalHost().getHostAddress();
+    }
+
+    static void copy(String src, String dst) throws IOException {
+        try (InputStream in = new FileInputStream(src);
+             OutputStream out = new FileOutputStream(dst)) {
+            byte[] buf = new byte[1];
+            int n;
+            while ((n = in.read(buf)) >= 0)
+                out.write(buf, 0, n);
+        }
     }
 
 }
