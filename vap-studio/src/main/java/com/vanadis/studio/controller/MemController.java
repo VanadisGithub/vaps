3739b2abeefd57005d8d60a2e755d38c90a7b6b1
yaoyuan <yy287502@alibaba-inc.com>
2023-08-28 10:43:20 +0800

    feature:test

--- vap-studio/src/main/java/com/vanadis/studio/controller/MemController.java
+++ vap-studio/src/main/java/com/vanadis/studio/controller/MemController.java
@@ -1,15 +1,24 @@
 package com.vanadis.studio.controller;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.alibaba.fastjson.JSONObject;
 
 import com.vanadis.lang.file.FileUtils;
 import com.vanadis.studio.entity.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Mem
@@ -32,5 +41,51 @@ public class MemController {
     @GetMapping("read")
     public JSONObject read() {
         return data.read();
+    }
+
+    @GetMapping("get")
+    public JSONObject get() {
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+//        httpRequestFactory.setConnectionRequestTimeout(3000);
+//        httpRequestFactory.setConnectTimeout(3000);
+//        httpRequestFactory.setReadTimeout(3000);
+//        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(httpRequestFactory);
+        return restTemplate.getForObject("http://127.0.0.1:8080/get1", JSONObject.class);
+    }
+
+    @GetMapping("get1")
+    public JSONObject get1() throws InterruptedException {
+        for (int i = 0; i < 6; i++) {
+            Thread.sleep(10000);
+            System.out.println(i + 1);
+        }
+        return new JSONObject();
+    }
+
+    private final List<String> dynamicStrings = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J"));
+
+    @GetMapping("/flux1")
+    public Flux<String> getInitialFlux() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .zipWith(Flux.fromIterable(dynamicStrings), (i, s) -> s);
+    }
+
+    @GetMapping("/flux2")
+    public Flux<String> getDynamicFlux() {
+        Flux<String> flux = Flux.fromIterable(dynamicStrings);
+        dynamicStrings.remove(0);
+        dynamicStrings.add(String.valueOf(System.currentTimeMillis()));
+        return flux;
+    }
+
+    @GetMapping("/reactive")
+    public Mono<List<String>> getReactiveData() {
+        Mono<String> mono1 = Mono.delay(Duration.ofSeconds(2)).map(i -> "Result 1");
+        Mono<String> mono2 = Mono.delay(Duration.ofSeconds(8)).map(i -> "Result 2");
+        Mono<String> mono3 = Mono.delay(Duration.ofSeconds(16)).map(i -> "Result 3");
+        System.out.println(1);
+        return Mono.zip(mono1, mono2, mono3)
+                .map(tuple -> Arrays.asList(tuple.getT1(), tuple.getT2(), tuple.getT3()));
     }
 }
